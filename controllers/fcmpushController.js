b3ec444564b218b9134cb7a9a1d806d90a406f9c
PeaceDev\jupiter <jupiter.fierce.31@gmail.com>
2018-10-17 22:11:51 +0800

    push notification

--- controllers/fcmpushController.js
+++ controllers/fcmpushController.js
@@ -6,6 +6,7 @@ var User = require('../models/User');
 var message = {
     to: 'etX589uc63E:APA91bHZkR-DzHVTNL8_YTlXqLGgBPWgMmSXeOzw3R-6KGUXi8BPAs6AOQxOFouCIlYQ7uAWugVgluxU1KRR_5MmjKStbcVSaXQvH9NxhJhoV8hLEW_he_93GrPb-4AJUYgobhkdzxE8',
     // to: 'cj5If-hiIxA:APA91bGBuRyirfq_2IxeUYErzv1mZ8xYneS263GIPcPxs-TP0Z2Yk0Ku_D4yv-FqtsaECmzgqhLnCfsHFNpaGwk-yeR8PzxPVLpG2A94kHZIiLgD8tivMIpw-jaPveidOlJH-vYIFQZN', // required fill with device token or topics
+//    to: 'epzMAWC2yvQ:APA91bFy8ealjxNbJ2kiizm2xCnNIslQgGN2D3WppMTw4GTclGlElIL6J3zg1ln71DENuHVUWtZ-ODdnXCwyhKZ3iMD_3Ze3M5uMI3nYp51yWonQ8kUeMFIPkUNbYGLqqoS1x2PHesXi'
     collapse_key: 'green',
     data: {
         your_custom_data_key: 'mydata'
@@ -13,7 +14,8 @@ var message = {
     notification:
     {
         title: 'Title of your push notification',
-        body: 'Body of your push notification'
+        body: 'Body of your push notification',
+        sound: 'default'
     }
 };
 
@@ -38,6 +40,7 @@ exports.push_all = (req, res) => {
         for (var user of users) {
             if (user.fcmToken) {
                 message.to = user.fcmToken;
+                console.log(user.name);
                 //callback style
                 fcm.send(message, function (err, response) {
                     if (err) {
@@ -54,6 +57,5 @@ exports.push_all = (req, res) => {
         res.send({ success: true, res: 'sent pushnotifications' });//success
        
     });
-
 }
 
